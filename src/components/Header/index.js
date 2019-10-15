67f95750e40dfab10ea01fbe64f7030fa54b2ffc
Denzel Hartshorn <denzelhartshorn@gmail.com>
2019-10-15 12:56:31 -0700

    polished Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,10 +4,9 @@ import "./style.css";
 function Header() {
   return (
     <header className="header">
-      <h1>Clicky Game!</h1>
-      <h2>
-        Click on an image to earn points, but don't click on any more than once!
-      </h2>
+      <h1>Memory Game!</h1>
+      <h2>Rules: Don't Click an Image Twice!</h2>
+      <h3>created with react by Denzel Hartshorn</h3>
     </header>
   );
 }
